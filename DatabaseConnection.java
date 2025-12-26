ad3eb9941942fb27954213db099770d3419e7af4
Aaditya-Kafle <akafle1129@gmail.com>
2025-12-26 13:27:17 +0545

    Payment completed

---
+++ DatabaseConnection.java
@@ -0,0 +1,18 @@
+package example;
+
+public class DatabaseConnection {
+	private boolean connected = false;
+
+    public void connectToDatabase() {
+        connected = true;
+    }
+
+    public void closeConnection() {
+        connected = false;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+}
