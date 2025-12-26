fa731e9ec19bb3ff388ee30c0facfae9cfb1232a
Aaditya-Kafle <akafle1129@gmail.com>
2025-12-26 12:54:49 +0545

    My file containing banking system

---
+++ BankingSystem.java
@@ -0,0 +1,18 @@
+package example;
+
+public class BankingSystem {
+	private double balance = 0;
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+}
